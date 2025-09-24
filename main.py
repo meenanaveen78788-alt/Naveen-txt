477da461e9264317fb1bde7e9ade4b1ba0861a35
nikhil1w <rayrambadan70@gmail.com>
2025-09-24 15:59:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1144,14 +1144,14 @@ async def txt_handler(bot: Client, m: Message):
          
             elif "https://cpvod.testbook.com/" in url or "classplusapp.com/drm/" in url:
                 url = url.replace("https://cpvod.testbook.com/","https://media-cdn.classplusapp.com/drm/")
-                url = f"https://marco-cp-api.vercel.app/marco?url={url}@ITSMARCOOFFICIAL&user_id={user_id}"
+                url = f"https://covercel.vercel.app/extract_keys?url={url}@bots_updatee&user_id={user_id}"
                 #url = f"https://scammer-keys.vercel.app/api?url={url}&token={cptoken}&auth=@scammer_botxz1"
                 mpd, keys = helper.get_mps_and_keys(url)
                 url = mpd
                 keys_string = " ".join([f"--key {key}" for key in keys])
 
             elif "classplusapp" in url:
-                signed_api = f"https://marco-cp-api.vercel.app/marco?url={url}@ITSMARCOOFFICIAL&user_id={user_id}"
+                signed_api = f"https://covercel.vercel.app/extract_keys?url={url}@bots_updatee&user_id={user_id}"
                 response = requests.get(signed_api, timeout=200)
                 url = response.text.strip()
                 url = response.json()['url']  
